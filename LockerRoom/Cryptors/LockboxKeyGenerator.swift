411fae92771c85352f8934fe74f10eda5f23331f
Nolan Astrein <nastrein@gmail.com>
2024-03-29 21:02:13 -0500

    Organizing some extensions.

--- LockerRoom/Cryptors/LockboxKeyGenerator.swift
+++ LockerRoom/Cryptors/LockboxKeyGenerator.swift
@@ -76,3 +76,60 @@ struct LockboxKeyGenerator {
         }
     }
 }
+
+private extension LockboxKey.Slot {
+    var pivSlot: PIVSlot {
+        switch self {
+        case .pivAuthentication:
+            return .authentication
+        case .digitalSignature:
+            return .signature
+        case .keyManagement:
+            return .keyManagement
+        case .cardAuthentication:
+            return .cardAuth
+        case .attestation:
+            return .attestation
+        case .experimental:
+            let experimentalRawValue: UInt8 = 0x82 // TODO: Results in EXC_BAD_ACCESS
+            return unsafeBitCast(experimentalRawValue, to: PIVSlot.self)
+        }
+    }    
+}
+
+private extension LockboxKey.Algorithm {
+    var pivKeyType: PIVKeyType {
+        switch self {
+        case .RSA1024:
+            return .RSA1024
+        case .RSA2048:
+            return .RSA2048
+        }
+    }
+}
+
+private extension LockboxKey.PinPolicy {
+    var pivPinPolicy: PIVPinPolicy {
+        switch self {
+        case .never:
+            return .never
+        case .once:
+            return .once
+        case .always:
+            return .always
+        }
+    }
+}
+
+private extension LockboxKey.TouchPolicy {
+    var pivTouchPolicy: PIVTouchPolicy {
+        switch self {
+        case .never:
+            return .never
+        case .always:
+            return .always
+        case .cached:
+            return .cached
+        }
+    }
+}
--- LockerRoom/Views/Models/LockerRoomLockboxKeyMetadata.swift
+++ LockerRoom/Views/Models/LockerRoomLockboxKeyMetadata.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-import YubiKit
-
 struct LockerRoomLockboxKeyMetadata: Identifiable {
     let id = UUID()
     let name: String
@@ -21,66 +19,17 @@ struct LockerRoomLockboxKeyMetadata: Identifiable {
 }
 
 extension LockboxKey.Slot: Identifiable {
-    var pivSlot: PIVSlot {
-        switch self {
-        case .pivAuthentication:
-            return .authentication
-        case .digitalSignature:
-            return .signature
-        case .keyManagement:
-            return .keyManagement
-        case .cardAuthentication:
-            return .cardAuth
-        case .attestation:
-            return .attestation
-        case .experimental:
-            let experimentalRawValue: UInt8 = 0x82 // TODO: Results in EXC_BAD_ACCESS
-            return unsafeBitCast(experimentalRawValue, to: PIVSlot.self)
-        }
-    }
-    
     var id: String { self.rawValue }
 }
 
 extension LockboxKey.Algorithm: Identifiable {
-    var pivKeyType: PIVKeyType {
-        switch self {
-        case .RSA1024:
-            return .RSA1024
-        case .RSA2048:
-            return .RSA2048
-        }
-    }
-    
     var id: String { self.rawValue }
 }
 
 extension LockboxKey.PinPolicy: Identifiable {
-    var pivPinPolicy: PIVPinPolicy {
-        switch self {
-        case .never:
-            return .never
-        case .once:
-            return .once
-        case .always:
-            return .always
-        }
-    }
-    
     var id: String { self.rawValue }
 }
 
 extension LockboxKey.TouchPolicy: Identifiable {
-    var pivTouchPolicy: PIVTouchPolicy {
-        switch self {
-        case .never:
-            return .never
-        case .always:
-            return .always
-        case .cached:
-            return .cached
-        }
-    }
-    
     var id: String { self.rawValue }
 }
